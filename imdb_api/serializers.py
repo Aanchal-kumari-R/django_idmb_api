d6343d1cf596570fb3b65f896db4477074f97d39
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-02 05:37:18 +0530

    Adding model Serializer

--- imdb_api/serializers.py
+++ imdb_api/serializers.py
@@ -29,29 +29,34 @@ class WatchListSerializers(serializers.Serializer):
 
 
 
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StreamPlatform  # Specify the model
+        fields = '__all__'  # or list the specific fields you want to include 
 
-class StreamPlatformSerializer(serializers.Serializer):   
+        
+# class StreamPlatformSerializer(serializers.Serializer):   
 
-    id = serializers.IntegerField(read_only=True)     
-    name = serializers.CharField(max_length=100)
-    about = serializers.CharField(max_length=200) 
-    website = serializers.URLField(max_length=200)   
+#     id = serializers.IntegerField(read_only=True)     
+#     name = serializers.CharField(max_length=100)
+#     about = serializers.CharField(max_length=200) 
+#     website = serializers.URLField(max_length=200)   
  
-    def create(self, validated_data):
-        """
-        Create and return a new `StreamPlatform` instance, given the validated data.
-        """
-        return WatchList.objects.create(**validated_data)   
+#     def create(self, validated_data):
+#         """
+#         Create and return a new `StreamPlatform` instance, given the validated data.
+#         """
+#         return WatchList.objects.create(**validated_data)   
 
-    def update(self, instance, validated_data):
-         """
-         Update and return an existing `StreamPlatform` instance, given the validated data.
-         """
-         instance.name = validated_data.get('name', instance.name)
-         instance.about = validated_data.get('about', instance.about)
-         instance.website = validated_data.get('website', instance.website)
-         instance.save()
-         return instance
+#     def update(self, instance, validated_data):
+#          """
+#          Update and return an existing `StreamPlatform` instance, given the validated data.
+#          """
+#          instance.name = validated_data.get('name', instance.name)
+#          instance.about = validated_data.get('about', instance.about)
+#          instance.website = validated_data.get('website', instance.website)
+#          instance.save()
+#          return instance
 
  
 
