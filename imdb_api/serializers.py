81c16827b11192788f37ecea3ca2f54cbb04155b
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-02 05:46:44 +0530

    Adding more model Serializer

--- imdb_api/serializers.py
+++ imdb_api/serializers.py
@@ -1,32 +1,10 @@
 from rest_framework import serializers 
-from .models import WatchList,StreamPlatform 
+from .models import WatchList,StreamPlatform  
 
-class WatchListSerializers(serializers.Serializer):  
-
-    id = serializers.IntegerField(read_only=True)    
-    title = serializers.CharField(max_length=100)
-    storyLine = serializers.CharField(max_length=200) 
-   # platform = serializers.ForeignKey(StreamPlatform,on_delete=serializers.CASCADE)  
-    active = serializers.BooleanField(default=True) 
-    created = serializers.DateTimeField()   
-
-    def create(self, validated_data):
-        """
-        Create and return a new `WatchList` instance, given the validated data.
-        """
-        return WatchList.objects.create(**validated_data)   
-    
-    def update(self, instance, validated_data):
-         """
-         Update and return an existing `WatchList` instance, given the validated data.
-         """
-         instance.title = validated_data.get('title', instance.title)
-         instance.storyLine = validated_data.get('storyLine', instance.storyLine)
-         instance.active = validated_data.get('active', instance.active)
-         instance.created = validated_data.get('created', instance.created)
-         instance.save()
-         return instance
-
+class WatchListSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = WatchList 
+        fields = '__all__'
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
--- imdb_api/views.py
+++ imdb_api/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from .models import WatchList , StreamPlatform 
-from .serializers import WatchListSerializers, StreamPlatformSerializer  
+from .serializers import WatchListSerializer, StreamPlatformSerializer  
 from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework.decorators import api_view
@@ -11,13 +11,13 @@ from rest_framework.decorators import api_view
 
 def movie_list(request):  
     movie_list = WatchList.objects.all() 
-    serialized = WatchListSerializers(movie_list,many=True)
+    serialized = WatchListSerializer(movie_list,many=True)
 
     return JsonResponse(serialized.data,safe=False)
 
 def movie_detail(request,pk):  
     movie = WatchList.objects.get(pk=pk)  
-    serialized = WatchListSerializers(movie)
+    serialized = WatchListSerializer(movie)
 
     return JsonResponse(serialized.data)  
 
