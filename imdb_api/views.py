d903f198f26f633056b877050f2e812921897ba0
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-04 12:30:51 +0530

    Creating an Endpoint for the root of API

--- imdb_api/views.py
+++ imdb_api/views.py
@@ -7,12 +7,20 @@ from rest_framework.decorators import api_view
 from django.http import Http404 
 from rest_framework.views import APIView 
 from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import generics 
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'WatchList': reverse('watch-list-view', request=request, format=format),
+        'StreamPlatform': reverse('stream-platform', request=request, format=format)
+    })
+
 
 class WatchListView(generics.ListCreateAPIView): 
     queryset = WatchList.objects.all()
     serializer_class = WatchListSerializer 
-
 
 class WatchListDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = WatchList.objects.all()
@@ -21,9 +29,6 @@ class WatchListDetail(generics.RetrieveUpdateDestroyAPIView):
 class StreamPlatformList(generics.ListCreateAPIView): 
     queryset = StreamPlatform.objects.all()
     serializer_class = StreamPlatformSerializer 
-
-
-
 
 class StreamPlatformDetails(generics.RetrieveUpdateDestroyAPIView): 
     queryset = StreamPlatform.objects.all()
