886f2b48181b7acd9844b559638a2513cf731a5d
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-05 12:32:02 +0530

    Routers

--- imdb_api/urls.py
+++ imdb_api/urls.py
@@ -1,38 +1,19 @@
-from django.urls import path  
+from django.urls import path  , include
 from . import views 
-from rest_framework.urlpatterns import format_suffix_patterns
-
-streamplatform_list = views.StreamPlatformViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-streamplatform_detail = views.StreamPlatformViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
- 
-watchlist_view = views.WatchListViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-watchlist_detail = views.WatchListViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+from rest_framework.urlpatterns import format_suffix_patterns 
+from rest_framework.routers import DefaultRouter
 
 
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()  
+router.register(r'stream', views.StreamPlatformViewSet, basename='streamplatform')
+router.register(r'list', views.WatchListViewSet, basename='watchlist')
 
 
-urlpatterns = [
-    path('list/',watchlist_view,name="watchlist_view"),
-    path('list/<int:pk>',watchlist_detail,name="watchlist_detail"),
-    path('stream/',streamplatform_list,name="streamplatform_list"),
-    path('stream/<int:pk>',streamplatform_detail,name="streamplatform_detail"), 
+urlpatterns = [ 
+    path('',include(router.urls)),
+    path('',include(router.urls)),
     path('', views.api_root),
 
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns)
