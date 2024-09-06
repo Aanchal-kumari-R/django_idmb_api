6a5db900d838b542221017641d0bdfad26c0d751
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-06 17:30:18 +0530

    more nested serializer

--- imdb_api/serializers.py
+++ imdb_api/serializers.py
@@ -8,7 +8,7 @@ class WatchListSerializer(serializers.ModelSerializer):
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):  
-    watch_list = WatchListSerializer(many=True,read_only=True)
+    watch_list = serializers.StringRelatedField(many=True,read_only=True) 
     class Meta:
         model = StreamPlatform  # Specify the model
         fields = '__all__'  # or list the specific fields you want to include 
